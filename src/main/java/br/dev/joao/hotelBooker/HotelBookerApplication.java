7c4e34f5857e1ab60f9e2545a56f11e8701097a9
Joao Henrique <joaoibitira@gmail.com>
2023-04-13 02:41:28 -0300

    Created maven repository

---
+++ src/main/java/br/dev/joao/hotelBooker/HotelBookerApplication.java
@@ -0,0 +1,13 @@
+package br.dev.joao.hotelBooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/joao/hotelBooker/HotelBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.joao.hotelBooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
